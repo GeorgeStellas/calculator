1794f44c5eb135a002bdf90a3bef755c8b05643d
GeorgeStellas <stemilanstellas1973@gmail.com>
2024-10-28 06:59:13 +0530

    designed calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, TextField} from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container width-60 mx-auto flex justify-center mt-10">
+      <div className='calculator w-80 bg-slate-500 p-5 rounded flex flex-wrap flex-start'>
+        <TextField id="outlined-basic" variant="outlined" className='bg-slate-50 w-full'/>
+        <div className='mt-2 mx-auto grid grid-cols-4 gap-2'> 
+            <Button variant="contained" className='w-1/4'>AC</Button>
+            <Button variant="contained" className='w-1/4'>C</Button>
+            <Button variant="contained" className='w-1/4'>%</Button>
+            <Button variant="contained" className='w-1/4'>/</Button>
+            <Button variant="contained" className='w-1/4'>7</Button>
+            <Button variant="contained" className='w-1/4'>8</Button>
+            <Button variant="contained" className='w-1/4'>8</Button>
+            <Button variant="contained" className='w-1/4'>*</Button>
+            <Button variant="contained" className='w-1/4'>4</Button>
+            <Button variant="contained" className='w-1/4'>5</Button>
+            <Button variant="contained" className='w-1/4'>6</Button>
+            <Button variant="contained" className='w-1/4'>-</Button>
+            <Button variant="contained" className='w-1/4'>1</Button>
+            <Button variant="contained" className='w-1/4'>2</Button>
+            <Button variant="contained" className='w-1/4'>3</Button>
+            <Button variant="contained" className='w-1/4'>+</Button>
+            <Button variant="contained" className='w-1/4 col-span-1'>.</Button>
+            <Button variant="contained" className='w-1/4 col-span-1'>0</Button>
+            <Button variant="contained" className='col-span-2'>EQUALS</Button>            
+        </div>
+        
+      </div>
     </div>
+
   );
 }
 
